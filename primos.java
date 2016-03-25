23ed2016d935ba48f73807881bb56a18931edcff
pchelpme12 <pchelpme12@gmail.com>
2016-03-25 02:56:51 +0000

    patched

---
+++ primos.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class primos{
+	
+	
+	static Scanner userInput = new Scanner(System.in);
+	
+	public static void main(String[] args){
+		System.out.println("numeros primos (patch das 2:40 da manhã :D)");
+		System.out.println("escolher um numero Maximo!");
+		if(userInput.hasNextInt()){
+			int max = userInput.nextInt();
+			for(int i=2;i<=max;i++){
+				for(int a=2;a<=i;a++){
+					if(a==i){
+						System.out.println(i);
+					}
+					if(i%a==0){
+						break;
+					}
+				}
+			}
+		}else{
+			System.out.println("next time type a number");
+		}
+	}
+}
